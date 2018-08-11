d8ca3c361e4d176afd78afb99598e7ad723f9c0b
chiranjeevi tapal <chiranjeevi.tapal@outlook.com>
2018-08-11 23:12:12 +0530

    added dotenv

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http);
+require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
-
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+   res.sendFile(__dirname + '/index.html');
 })
 
 io.on('connection', (socket) => {
