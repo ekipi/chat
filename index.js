058bc57435f859dee8237a720138f18192a11a4b
chiranjeevi tapal <chiranjeevi.tapal@outlook.com>
2018-08-12 01:44:01 +0530

    added logic for room broadcast chat. Only users in same room can see the chat messages

--- index.js
+++ index.js
@@ -6,16 +6,25 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-   res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 })
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('chat message', (message) => {
-        io.emit('chat message', message)
+    let client_id = socket.id
+    console.log(`${client_id} connected`);
+    // once a client has connected, get the room id from client
+    let room = ''
+    socket.on('room', (room_id) => {
+        socket.join(room_id);
+        room = room_id; //store the room id so that messages could be broadcasted to that perticular room
+    });
+    //When client emits the chat event, capture the chat message and broadcast it to the room
+    socket.on('chat', (message) => {
+        io.sockets.in(room).emit('chat', client_id, message);
     })
+    //Upon disconnection, display a message in the log
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log(`${client_id} disconnected`);
     });
 });
 
