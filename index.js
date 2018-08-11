9ab78dd3635f5753df48927ad2cd75cf0de99d13
chiranjeevi tapal <chiranjeevi.tapal@outlook.com>
2018-08-11 22:52:58 +0530

    basic broadcast chat

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', (message) => {
+        io.emit('chat message', message)
+    })
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+})
